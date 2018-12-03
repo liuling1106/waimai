27bce019085e07a31eaf47e493db7fcb56041a29
xiaojing.liu <xiaojing.liu@us.beyondsoft.com>
2018-12-03 10:23:25 +0800

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="header">
+<h1>{{msg}}</h1>
+</div>
+</template>
+<script>
+export default {
+  name: "",
+  props: {
+    msg: "Hello"
+  }
+};
+</script>
+<style scoped>
+</style>
